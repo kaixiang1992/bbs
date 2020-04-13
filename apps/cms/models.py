d154827d7eb2c0bfcbabdd91c6efd4ddde9d55c2
kaixiang <49449720+kaixiang1992@users.noreply.github.com>
2020-04-13 23:55:08 +0800

    Flask项目实战权限和角色模型定义、功能完成

--- apps/cms/models.py
+++ apps/cms/models.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
+# TODO: 用户类
 class CmsUser(db.Model):
     __tablename__ = 'cms_user'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -31,9 +32,87 @@ class CmsUser(db.Model):
         result = check_password_hash(pwhash=self.password, password=raw_password)
         return result
 
+    # TODO: 获取用户所有权限
+    @property
+    def permissions(self):
+        if not self.roles:
+            return 0
+        all_permissions = 0
+        # TODO: 遍历用户所有权限
+        for role in self.roles:
+            permission = role.permissions
+            # TODO: 或运算：0|1=1 0|0=0 相当于Python if条件中的or
+            all_permissions |= permission
+        return all_permissions
+
+    # TODO: 验证用户是否拥有某个权限
+    def has_permission(self, permission):
+        # permissions = self.permissions
+        # TODO: 与运算：0&1=0 1&1=1 相当于python if条件中的and
+        # result = permissions & permission == permission
+        # return result
+
+        return self.permissions & permission == permission
+
+    # TODO: 验证用户是否为开发者
+    @property
+    def is_developer(self):
+        return self.has_permission(CMSPersmission.ALL_PERMISSION)
+
+    def __repr__(self):
+        return '<CmsUser(username=%s, email=%s)>' % (self.username, self.email)
+
 # user = CMSUser()
 # print(user.password)
 # user.password = 'abc'
 
 # 密码：对外的字段名叫做password
 # 密码：对内的字段名叫做_password
+
+
+# TODO: 权限列表
+class CMSPersmission(object):
+    # 255的二进制方式来表示 1111 1111
+    ALL_PERMISSION = 0b11111111
+    # 1. 访问者权限
+    VISITOR = 0b00000001
+    # 2. 管理帖子权限
+    POSTER = 0b00000010
+    # 3. 管理评论的权限
+    COMMENTER = 0b00000100
+    # 4. 管理板块的权限
+    BOARDER = 0b00001000
+    # 5. 管理前台用户的权限
+    FRONTUSER = 0b00010000
+    # 6. 管理后台用户的权限
+    CMSUSER = 0b00100000
+    # 7. 管理后台管理员的权限
+    ADMINER = 0b01000000
+
+
+# TODO: 用户权限中间关联表
+cms_users_role = db.Table(
+    'cms_users_role',
+    db.Column('id', db.Integer, primary_key=True, autoincrement=True, default=1),
+    db.Column('cms_role_id', db.Integer, db.ForeignKey('cms_role.id'), primary_key=True),
+    db.Column('cms_user_id', db.Integer, db.ForeignKey('cms_user.id'), primary_key=True),
+    db.Column('create_time', db.DATETIME, default=datetime.now)
+)
+
+
+# TODO: 权限角色
+class CmsRole(db.Model):
+    __tablename__ = 'cms_role'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(30), nullable=False)
+    desc = db.Column(db.TEXT, nullable=True)
+    # TODO: 默认访问者权限
+    permissions = db.Column(db.Integer, default=CMSPersmission.VISITOR)
+    create_time = db.Column(db.DATETIME, default=datetime.now)
+
+    users = db.relationship('CmsUser', secondary=cms_users_role, backref=db.backref('roles', uselist=True),
+                            uselist=True)
+
+
+    def __repr__(self):
+        return '<CmsRole(name=%s, desc=%s)>' % (self.name, self.desc)
--- manage.py
+++ manage.py
@@ -1,11 +1,11 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from app import app
+from app import create_app
 from exts import db
 from apps.cms import models as cms_model
 
-manager = Manager(app=app)
-Migrate(app=app, db=db)
+manager = Manager(app=create_app())
+Migrate(app=create_app(), db=db)
 manager.add_command('db', MigrateCommand)
 
 
@@ -18,6 +18,73 @@ def create_default_user(username, password, email):
     db.session.add(cms_user)
     db.session.commit()
     print('初始化系统用户成功...')
+
+
+# TODO: 初始化创建权限数据
+@manager.command
+def create_default_roles():
+    # 1. 访问者（可以修改个人信息）
+    visitor = cms_model.CmsRole(name='访问者', desc='只能相关数据，不能修改。')
+    visitor.permissions = cms_model.CMSPersmission.VISITOR
+
+    # 2. 运营角色（修改个人个人信息，管理帖子，管理评论，管理前台用户）
+    operator = cms_model.CmsRole(name='运营', desc='管理帖子，管理评论,管理前台用户。')
+    operator.permissions = cms_model.CMSPersmission.VISITOR | cms_model.CMSPersmission.POSTER \
+                           | cms_model.CMSPersmission.CMSUSER | cms_model.CMSPersmission.COMMENTER \
+                           | cms_model.CMSPersmission.FRONTUSER
+
+    # 3. 管理员（拥有绝大部分权限）
+    admin = cms_model.CmsRole(name='管理员', desc='拥有本系统所有权限。')
+    admin.permissions = cms_model.CMSPersmission.VISITOR | cms_model.CMSPersmission.POSTER | \
+                        cms_model.CMSPersmission.CMSUSER | cms_model.CMSPersmission.COMMENTER | \
+                        cms_model.CMSPersmission.FRONTUSER | cms_model.CMSPersmission.BOARDER
+
+    # 4. 开发者
+    developer = cms_model.CmsRole(name='开发者', desc='开发人员专用角色。')
+    developer.permissions = cms_model.CMSPersmission.ALL_PERMISSION
+
+    db.session.add_all([visitor, operator, admin, developer])
+    db.session.commit()
+
+
+# TODO: 初始化用户权限
+@manager.option('-e', '--email', dest="email")
+@manager.option('-i', '--id', dest='id')
+def add_user_role(email, id):
+    user = cms_model.CmsUser.query.filter_by(email=email).one_or_none()
+    print(user)
+    if user:
+        role = cms_model.CmsRole.query.get(id)
+        print(role)
+        if role:
+            role.users.append(user)
+            db.session.add(role)
+            db.session.commit()
+            print('%s 用户权限添加成功！' % email)
+        else:
+            print('id为：%s，角色不存在' % id)
+    else:
+        print('%s 邮箱不存在！' % email)
+
+
+# TODO: 测试用户是否拥有某个权限
+@manager.option('-e', '--email', dest='email')
+@manager.option('-i', '--id', dest='id')
+def test_user_permission(email, id):
+    user = cms_model.CmsUser.query.filter_by(email=email).one_or_none()
+    print(user)
+    if user:
+        role = cms_model.CmsRole.query.get(id)
+        print(role)
+        if role:
+            if user.has_permission(permission=role.permissions):
+                print('%s 权限，%s 该用户拥有！' % (role.name, user.email))
+            else:
+                print('%s 权限，%s 该用户权限不足！' % (role.name, user.email))
+        else:
+            print('id为：%s，角色不存在' % id)
+    else:
+        print('%s 邮箱不存在！' % email)
 
 
 if __name__ == '__main__':
