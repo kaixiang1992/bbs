bdce97c2c637a5b0678bceccfdf2296db961560f
kaixiang <49449720+kaixiang1992@users.noreply.github.com>
2020-04-21 23:33:59 +0800

    图形验证码生成技术详解

--- apps/front/views.py
+++ apps/front/views.py
@@ -1,4 +1,12 @@
-from flask import (Blueprint, views, render_template)
+from flask import (
+    Blueprint,
+    views,
+    render_template,
+    make_response
+)
+from untils.captcha import Captcha
+from io import BytesIO
+
 
 bp = Blueprint('front', __name__)
 
@@ -12,6 +20,21 @@ def homepage():
 class SignupView(views.MethodView):
     def get(self):
         return render_template('front/front_signup.html')
+
+
+# TODO: 图形验证码视图
+@bp.route('/captcha/')
+def CaptchaView():
+    text, image = Captcha.gene_graph_captcha()
+    out = BytesIO()
+    # TODO: 将图片保存到IO中格式png
+    image.save(out, 'png')
+    # TODO: 保存完毕后，移动指针到起始位置
+    out.seek(0)
+    # TODO: 将IO读取出来转为image/png响应
+    resp = make_response(out.read())
+    resp.content_type = 'image/png'
+    return resp
 
 
 # TODO: 注册页面视图
